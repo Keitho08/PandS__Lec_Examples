d239eb7d6b464512938bcbbfc9d8af93cc11171f
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-02-17 12:23:47 +0000

    Made code more efficient

--- primes.py
+++ primes.py
@@ -9,14 +9,14 @@ P = []
 for i in range(2, 1000):
     #Assume i is prime
     isprime = True 
-    #Loop through all values from 2 up to but not including i
-    for j in range(2, i):
+    #Loop through only generated prime numbers faster than all number in 1st revision
+    for j in P:
         #See if j divides i
         if i % j == 0:
             # if it does i isn't prime and exit loop
             isprime = False
             break
-    # If it is prime, then append to P.
+    # If it is prime, then append to P
     if isprime:
       P.append(i)     
 
